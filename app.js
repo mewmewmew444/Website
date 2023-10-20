6d20f0560c5f5c64cdf4787880e83d544ca9499f
mewmewmew444 <marianekaterina2014@gmail.com>
2023-10-20 14:11:33 +1000

    kotiki

---
+++ app.js
@@ -0,0 +1,17 @@
+VANTA.BIRDS({
+    el: "#mew",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0x0,
+    color1: 0x938b16,
+    color2: 0x75a200,
+    wingSpan: 17.00,
+    separation: 100.00,
+    alignment: 10.00,
+    cohesion: 1.00
+  })
